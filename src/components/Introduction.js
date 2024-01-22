1e540d699bcc381d49c7ff64558a84879e95d091
Kevin <kevinjunchan@gmail.com>
2024-01-22 08:39:06 +0000

    font and intro change

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -3,7 +3,7 @@ import '../css/Introduction.css'
 const Introduction = () => {
     return(
         <div className="container">  
-            <p>Hello! I'm</p>
+            {/* <p></p> */}
             <section class="animation">
                 <div class="first"><div>KEVIN CHAN</div></div>
                 <div class="second"><div> an Astrophysicist,</div></div>
